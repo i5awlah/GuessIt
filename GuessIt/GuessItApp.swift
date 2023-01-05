038327e8678f4e63fca11bf3957e51b077d065b3
Khawlah <khawlah_alrashed@hotmail.com>
2023-01-05 11:48:48 +0300

    Initial Commit

---
+++ GuessIt/GuessItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessItApp.swift
+//  GuessIt
+//
+//  Created by Khawlah on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
