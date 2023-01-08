8d14bc416da1de8fdaf06db3c831dc2e438d52b7
Khawlah <khawlah_alrashed@hotmail.com>
2023-01-08 10:53:54 +0300

    add color extension

---
+++ GuessIt/Utilities/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  GuessIt
+//
+//  Created by Khawlah on 08/01/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customOrange = Color("customOrange")
+    static let darkGreen = Color("darkGreen")
+    static let lightGreen = Color("lightGreen")
+    
+    static let babyLavender = Color("babyLavender")
+    static let darkLavender = Color("darkLavender")
+    static let letterLavender = Color("letterLavender")
+    static let lightLavender = Color("lightLavender")
+    static let navbarColor = Color("navbarColor")
+    
+    static let babyYellow = Color("babyYellow")
+    static let coinYellow = Color("coinYellow")
+    static let darkYellow = Color("darkYellow")
+    static let lightYellow = Color("lightYellow")
+}
