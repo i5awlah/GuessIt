2770b4134c4e87087ffee24c44d6a098f5d20750
Ameera Alsweed <Aialsweed22@twq.idserve.net>
2023-01-09 12:00:36 +0300

    StartPage WinPage (Not Completed)

--- GuessIt/View/StartView.swift
+++ GuessIt/View/StartView.swift
@@ -9,12 +9,43 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            (AngularGradient (
+                gradient: Gradient(colors: [Color(.gray)]),
+                           center: .topLeading,
+                           angle: .degrees(180+45)))
+             .ignoresSafeArea()
+            
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .frame (width: 267, height:327)
+                    .padding(100)
+                
+                    
+                
+
+                Button (
+                    action: {}, label: {
+                Text ("Play")
+                .font(.title2.weight (.semibold))
+                .foregroundColor (.white)
+                .frame (width: 265, height:44) .background (Color.customOrange)
+                .mask(RoundedRectangle (cornerRadius: 16, style: .continuous))
+
+                }
+                        )
+//                Image(systemName: "play.fill")
+//                    .foregroundColor (.white)
+//                    .frame (width: 197, height:44)
+            }
+            
+        }
     }
-}
 
 struct StartView_Previews: PreviewProvider {
     static var previews: some View {
         StartView()
     }
 }
+                }
--- GuessIt/View/WinView.swift
+++ GuessIt/View/WinView.swift
@@ -9,8 +9,96 @@ import SwiftUI
 
 struct WinView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        ZStack{
+            
+            Image("bg_sunburst")
+                .background (Color.babyLavender)
+                .ignoresSafeArea()
+            
+            VStack{
+                
+//                Circle()
+//                    .fill(Color.customOrange)
+//                    .frame(width: 100, height: 100)
+                
+                
+                Rectangle()
+                    .fill(Color.babyYellow)
+                    .frame(width: 348, height: 305)
+                    .mask(RoundedRectangle (cornerRadius: 16, style: .continuous))
+                
+                
+                        .overlay(alignment: .top) {
+                            Circle()
+                        .frame(width: 90, height: 96)
+                        .offset(y: -48)
+                        .overlay {
+                    Image(systemName: "checkmark")
+                    .foregroundColor (.white)
+                    .frame(width: 67, height: 77)
+                    .offset(y: -45)
+                        }
+                            }
+                
+//                Image(systemName: "checkmark")
+//                    .foregroundColor (.white)
+                
+                        .overlay(Text("Excellent !"), alignment: .center)
+                        .font(.title.weight (.bold))
+                        .foregroundColor (Color.customOrange)
+                
+                
+                
+                
+                        .overlay(alignment: .center){
+                            Rectangle()
+                                .fill(Color.customOrange)
+                                .frame(width: 210, height: 130)
+                                .mask(   RoundedRectangle(cornerRadius: 16)
+                        .stroke((Color.white), lineWidth: 3)
+                                )   }
+                
+                
+                
+                
+//                                .frame(width: 210, height: 132)
+//                                .mask(RoundedRectangle (cornerRadius: 16, style: .continuous, (Color.customOrange))
+                               // .border(Color.customOrange)
+                   
+                
+                
+                Button (action: {}, label: {
+                Text ("Next Level")
+                .font(.title2.weight (.semibold))
+                .foregroundColor (.white)
+                .frame (width: 265, height:44) .background (Color.customOrange)
+                .mask(RoundedRectangle (cornerRadius: 16, style: .continuous))
+
+                })
+
+                Button (action: {}, label: {
+                Text ("Quit")
+                .font(.title2.weight (.semibold))
+                .foregroundColor (.white)
+                .frame (width: 265, height:44) .background (Color.customOrange)
+                .mask(RoundedRectangle (cornerRadius: 16, style: .continuous))
+
+                })
+                        
+            }
+           
+//            Image(systemName: "checkmark")
+//              .resizable()
+//              .frame(width: 80, height: 86)
+//              .foregroundColor(.white)
+//              .padding(20)
+//              .background(Color.customOrange)
+//              .clipShape(Circle())
+//              .offset(y: -200)
+//
+            
+        }
+        }
 }
 
 struct WinView_Previews: PreviewProvider {
